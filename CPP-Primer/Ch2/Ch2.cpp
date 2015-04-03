10a7de9052b41b5308df82519b37992137aba5b7
CTai <glaxy.tai@gmail.com>
2015-04-03 11:47:25 +0800

    P83

--- CPP-Primer/Ch2/Ch2.cpp
+++ CPP-Primer/Ch2/Ch2.cpp
@@ -9,8 +9,12 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+int jj;
+extern double pi = 3.1416;
+int f = 42;
 int _tmain(int argc, _TCHAR* argv[])
 {
+	/*
 	int i = -42;
 	unsigned u = 10;
 	std::cout << i + i << std::endl;
@@ -21,6 +25,33 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << u2 - u1 << endl;
 	cout << "HI \x4dO\115!" << endl;
 	cout << "\115\n";
+	*/
+	int units_sold0 = 0;
+	int units_sold1 = {1};
+	int units_sold2(2);
+	int units_sold3{3};
+
+	long double ld = 3.1415926536;
+	// int a{ld}; // error C2397: conversion from 'long double' to 'int' requires a narrowing conversion
+	int b(ld);
+	int c = ld;
+	
+	extern int i; // declares but does not define i
+	int j; // declares and defines j
+	
+//	printf("i = %d", i); //error LNK2001: unresolved external symbol "int i" (?i@@3HA)
+//	printf("j = %d", j); //rutime check error
+	printf("jj = %d", jj);
+
+// 	extern double pi = 3.1416; //error C2205: 'pi' : cannot initialize extern variables with block scope  
+	int f = 100;
+	int z = ::f;
+
+	int ival = 1024;
+	int &refval = ival;
+//	int &refval2; //error C2530: 'refval2' : references must be initialized
+
+
 	return 0;
 }
 
