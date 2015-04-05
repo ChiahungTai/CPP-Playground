65458e0eb81708825818548c79a46c7e1910b137
CTai <glaxy.tai@gmail.com>
2015-04-05 23:25:08 +0800

    Ch2.6 done

--- CPP-Primer/Ch2/Ch2.cpp
+++ CPP-Primer/Ch2/Ch2.cpp
@@ -9,9 +9,14 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+
+class Sales_item {
+};
+
 int jj;
 extern double pi = 3.1416;
 int f = 42;
+int i = 2;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	/*
@@ -65,6 +70,34 @@ int _tmain(int argc, _TCHAR* argv[])
 	constexpr const int *p = &i; // p is a constant pointer to the const int i
 	constexpr int *p1 = &j; // p1 is a constant pointer to the int j
 	*/
+
+  typedef double wages; // wages is a synonym for double
+  typedef wages base, *p; // base is a synonym for double, p for double*
+
+  p pointDouble = new double[20];
+  delete pointDouble;
+
+  using SI = Sales_item;
+  wages hourly, weekly;
+  SI item;
+
+  const int ci = i, &cr = ci;
+  auto bi = ci;
+  auto co = cr;
+  auto d = &i;
+  auto e = &ci;
+
+  auto &g = ci; // g is a const int& that is bound to ci
+  // auto &h = 42; // error: we can't bind a plain reference to a literal
+  const auto &j = 42; // ok: we can bind a const reference to a literal
+
+  auto k = ci, &l = i; // k is int; l is int&
+  auto &m = ci, *pig = &ci; // m is a const int&;p is a pointer to const int
+  // error: type deduced from i is int; type deduced from &ci is const int
+  // IntelliSense: 'auto' type is "const int" for this entity, but was previously implied to be "int"
+  // auto &n = i, *p2 = &ci; //error C3538: in a declarator-list 'auto' must always deduce to the same type
+
+
 	return 0;
 }
 
